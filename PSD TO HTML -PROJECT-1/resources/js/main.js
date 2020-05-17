ec63f706e6718515907a1705071f276a026c1dc8
lipinaz <65477366+lipinaz@users.noreply.github.com>
2020-05-17 09:34:41 +0600

    added html CSS javascript  files for nazproject

---
+++ PSD TO HTML -PROJECT-1/resources/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    
+   //sticky menu
+    
+$(".js--service-section").waypoint(function(direction){
+
+if(direction=="down"){
+   
+    $("nav").addClass("sticky");
+    
+   
+    } else {
+    $("nav").removeClass("sticky");      
+   
+   }
+    
+    
+    
+    
+    
+});
+   
+   
+   
+    
+    
+    
+    
+    //mixitup
+  var mixer = mixitup('.container');
+
+
+});
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
